df57ab840c75b64fc4bd9045ade3bf7408d738dc
Hritik Sharma <sharmahritik2002@gmail.com>
2023-02-02 23:42:53 +0530

    Add location array

---
+++ front-end/lib/location.js
@@ -0,0 +1,32 @@
+export const locationArr = [
+  {
+    label: "Mumbai",
+  },
+  {
+    label: "Bandra",
+  },
+  {
+    label: "Malad",
+  },
+  {
+    label: "Thane",
+  },
+  {
+    label: "Banglore",
+  },
+  {
+    label: "Goregaon",
+  },
+  {
+    label: "KharRoad",
+  },
+  {
+    label: "Vasai",
+  },
+  {
+    label: "Andheri",
+  },
+  {
+    label: "Virar",
+  },
+];
