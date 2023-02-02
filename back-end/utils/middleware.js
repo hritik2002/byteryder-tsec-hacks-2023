34cd96f56726b28836da169fd9cadcff9273dd14
Rashid Aziz <md.r.a.n.786@gmail.com>
2023-02-02 15:44:54 +0530

    login completed

---
+++ back-end/utils/middleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "ROOMIIEEHOMMMIIEE";
+
+const auth = (req, res, next) => {
+    const authToken = req.header("authToken");
+    if(!authToken) {
+        return res.status(401).send({
+            error : "Please authenticate with valid token"
+        })
+    }
+    try {
+        const data = jwt.verify(authToken, JWT_SECRET)
+        req.user = data.user;
+        next();
+    } catch (error) {
+        // console.log(error);
+        res.status(401).send({
+            error : "Some error occured"
+        })
+    }
+}
+
+module.exports = auth;
