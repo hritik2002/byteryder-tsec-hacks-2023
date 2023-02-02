ce87778df0c550b2508972090e6e2f27c2039dbf
Prajwal <prajwaldhule36@gmail.com>
2023-02-02 16:32:27 +0530

    hero structure done (navbar issue)

--- front-end/pages/HomePage.jsx
+++ front-end/pages/HomePage.jsx
@@ -3,6 +3,8 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+import Navbar from "@/ui/Navbar";
+
 const HomePage = () => {
   useEffect(() => {
     // let tl1 = gsap.timeline({
@@ -23,26 +25,29 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="relative opacity-0 hero-wrapper box-border overflow-x-hidden flex justify-between min-w-[100vw] h-[100vh] items-center sm:px-[5vw]">
-      <div className="w-3/5">
-        <h1 className="text-5xl font-bold pb-6">
-          Find the perfect roommates in the place{" "}
-        </h1>
-        <p className="pb-6 w-4/5">
-          is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry's standard dummy text{" "}
-        </p>
-        <div className="btn-wrapper w-full flex justify-left pt-[2em]">
-          <button className="py-[1em] px-[2.5em] rounded bg-[#FEF3D3] shadow-2xl font-medium hover:bg-[#FFE395]">
-            Find rooms
-          </button>
-          <button className="ml-[4em] cursor-pointer py-[1em] px-[2.5em] rounded bg-[#FEF3D3] shadow-2xl font-medium hover:bg-[#FFE395]">
-            Find room mates
-          </button>
+    <div className="home-body">
+      <Navbar />
+      <div className="relative opacity-0 hero-wrapper box-border overflow-x-hidden flex justify-between min-w-[100vw] min-h-[100vh] items-center sm:px-[5vw]">
+        <div className="w-3/5">
+          <h1 className="text-5xl font-bold pb-6">
+            Find the perfect roommates in the place
+          </h1>
+          <p className="pb-6 w-4/5">
+            is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text
+          </p>
+          <div className="btn-wrapper w-full flex justify-left pt-[2em]">
+            <button className="cursor-pointer py-[1em] px-[2.5em] rounded bg-[#FEF3D3] shadow-2xl font-medium hover:bg-[#FFE395]">
+              Find rooms
+            </button>
+            <button className="ml-[4em] cursor-pointer py-[1em] px-[2.5em] rounded bg-[#FEF3D3] shadow-2xl font-medium hover:bg-[#FFE395]">
+              Find room mates
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="img-wrapper absolute right-[5vw] w-[36%] translate-y-[15vh]">
-        <img className="w-11/12" src="/hero-img-2.png"></img>
+        <div className="img-wrapper absolute right-[5vw] w-[36%] translate-y-[20vh]">
+          <img className="w-11/12" src="/hero-img-2.png"></img>
+        </div>
       </div>
     </div>
   );
--- front-end/ui/Navbar.jsx
+++ front-end/ui/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="nav-wrapper absolute top-0 z-10 py-[1em] flex items-center justify-between mx-[5vw] w-[90vw]">
+      <div className="w-full relative flex justify-between lg:w-auto">
+        <img src="/logo.png" className="h-[4em]" />
+      </div>
+      <div className="center-nav flex items-center justify-self-center">
+        <ul className="flex flex-row list-none lg:ml-auto">
+          <li className="nav-item cursor-pointer hover:opacity-50">
+            <a>Find room/roommates</a>
+          </li>
+          <li className="nav-item ml-[3em] cursor-pointer hover:opacity-50">
+            <a>About us</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="flex">
+          <li className="nav-item cursor-pointer hover:opacity-50">
+            <a>Log in</a>
+          </li>
+          <li className="nav-item ml-[3em] cursor-pointer hover:opacity-50">
+            <a>Register</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
